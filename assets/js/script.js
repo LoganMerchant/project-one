e41d3b5de44bad22871a02f4940a8e2e53f86704
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-09-10 21:07:00 -0600

    api-working

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,20 +11,20 @@
 
 //   })
 // var url =
-  // "http://newsapi.org/v2/everything?" +
-  // "q=Apple&" +
-  // "from=2020-09-10&" +
-  // "sortBy=popularity&" +
-  // "apiKey=7aa4007d232b4971be8c8b229d1ca299";
+// "http://newsapi.org/v2/everything?" +
+// "q=Apple&" +
+// "from=2020-09-10&" +
+// "sortBy=popularity&" +
+// "apiKey=7aa4007d232b4971be8c8b229d1ca299";
 
 // function myFunction() {
-  // var searchTerm = document.querySelector("format-control").value;
+// var searchTerm = document.querySelector("format-control").value;
 
 //   var req = new Request(url);
 
 //   fetch(req).then(function (response) {
 //     console.log(response.json());
-    
+
 //   })
 //   .then(function(response) {
 //     console.log(response.data[0]);
@@ -40,13 +40,14 @@
 var button = $(".btn");
 var newsSearch = $(".form-control").val();
 var list = JSON.parse(localStorage.getItem("headlines")) || [];
+var key = `pZwZYOOhZZTfRVT0WHKsRuANSpS6D0wl`;
 
 $(button).on("click", function (event) {
   event.preventDefault();
-  
+
   newsSearch = $(".form-control").val();
   $("form-control").empty();
-  console.log(newsSearch)
+  console.log(newsSearch);
 
   if (newsSearch === "") {
     alert("input a topic please");
@@ -56,20 +57,25 @@ $(button).on("click", function (event) {
 
     getNews(newsSearch);
   }
-})
-function getNews(){
-
-  // var searchTerm = document.querySelector('.form-control').value;
-
-  fetch(`https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/everything?q=${newsSearch}&apiKey=7aa4007d232b4971be8c8b229d1ca299`)
- 
-
-  .then(a => a.json())
-  .then(response=> {
-    console.log(response)
-    for(var i = 0; i < response.articles.length; i++ ){
-      document.getElementById("output").innerHTML += "<div style='padding-top: 20px;'><img style='float: left; width: 150px:' src='" + response.articles[i].urlToImage + "'><h1>"+response.articles[i].title + "</h1>" + response.articles[i].source.name + "<br>"+response.articles[i].description+" <a href='"+response.articles[i].url+"'target='_blank'>"+response.articles[i].url+"</a></div>";
-      }
+});
+function getNews() {
+  fetch(
+    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${newsSearch}&api-key=${key}`
+  )
+    .then(function (response) {
+      var data = response.json();
+      return data;
+      console.log(data);
     })
-  }
- 
+    .then(function (data) {
+      console.log(data);
+      // need to create an <img place for the picture
+      // var picture = data.response.docs[0].multimedia[0].url;
+      var headline = data.response.docs[0].headline.main;
+      var firstPara = data.response.docs[0].lead_paragraph
+      
+      $("#output-h1").append(headline);
+      $("#output-h2").append(firstPara);
+      
+    });
+}
