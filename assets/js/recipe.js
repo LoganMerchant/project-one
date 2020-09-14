32955ad9941d29a44ac7aac26e9c682707050d2f
Jani Muhlestein <janimuhlestein@gmail.com>
2020-09-13 23:34:12 -0600

    changed layout due to image issues

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,4 +1,4 @@
-//todo write all code
+//todo: make it prettier
 
 var container = document.querySelector("#container");
 
@@ -39,53 +39,62 @@ if(response.ok) {
         if(response.ok) {
             response.json().then(function(data){ 
                 console.log(data);
-                var img = document.createElement("img");
-                img.setAttribute("src", data.image);
-                img.setAttribute("class", "recipe-img");
-                container.appendChild(img);
-                var desc = data.summary;
-                var summary = document.createElement("p");
-                summary.setAttribute("class", "recipe-description");
-                summary.innerHTML = desc;
-                container.appendChild(summary);
-                var ingText = document.createElement("p");
-                ingText.setAttribute("class", "ingredient-header");
-                ingText.textContent = "Ingredients";
-                summary.appendChild(ingText);
-                var table = document.createElement("table"); 
-                table.border = '1'; //extendedIngredients[0].originalString
-                table.setAttribute("style", "width: 100%");
-                var tableBody = document.createElement("tbody");
-                table.appendChild(tableBody);
-                var ingredients = data.extendedIngredients;
-                var numRows = Math.floor(ingredients.length /2); //extendedIngredients[0].originalString extendedIngredients
-                for(i=0; i<numRows; i++) {
-                    var tr = document.createElement("tr");
-                    tableBody.appendChild(tr);
+               //put the description in
+               var desc = data.summary;
+               var description = document.createElement("h4");
+               description.setAttribute("class", "recipe-description");
+               description.innerHTML = desc;
+               container.appendChild(description);
+               var ingTitle = document.createElement("p");
+               ingTitle.setAttribute("class", "ingredient-header");
+               ingTitle.textContent = "Ingredients";
+               container.appendChild(ingTitle);
+               //create a new container for the ingredients, and image
+               var ingDiv = document.createElement("div");
+               ingDiv.setAttribute("class", "recipe-div");
+               var imgLink = data.image;
+               var img = document.createElement("img");
+               img.setAttribute("class", "recipe-img");
+               img.setAttribute("src", imgLink);
+               ingDiv.appendChild(img);
+               var list1Div = document.createElement("div");
+                list1Div.setAttribute("class", "recipe-ingredients");
+                var list2Div = document.createElement("div");
+                list2Div.setAttribute("class", "recipe-ingredients"); //extendedIngredients[0].originalString
+                var ingList = data.extendedIngredients;
+                var list1 = document.createElement("ul");
+                list1.setAttribute("class", "recipe-ingredients");
+                var numRows = Math.ceil(ingList.length / 2);
+                for(var i = 0; i< numRows; i++) {
+                    var item = document.createElement("li");
+                    item.textContent = ingList[i].originalString;
+                    list1Div.appendChild(item);
                 }
-                for (var j = 0; j< ingredients.length; j++) {
-                    var ing = ingredients[j].originalString;
-                    var td = document.createElement("td");
-                    td.width = '100px';
-                    td.setAttribute("border", "1px solid black");
-                    td.appendChild(document.createTextNode(ing));
-                    tr.appendChild(td);
+                ingDiv.appendChild(list1Div);
+                for(i = numRows + 1; i<ingList.length; i++) {
+                    var item1 = document.createElement("li");
+                    item1.textContent = ingList[i].originalString;
+                    list2Div.appendChild(item1);
                 }
-                summary.appendChild(table);
-                var insText = document.createElement("p");
-                insText.setAttribute("class", "recipe-instruction-header");
-                insText.textContent = "Instructions";
-                summary.appendChild(insText); //analyzedInstructions[0].steps analyzedInstructions[0].steps[0].step
-                var instruc = data.analyzedInstructions[0].steps;
-                var list = document.createElement("ul");
-                list.setAttribute("class", "group-list");
-                summary.appendChild(list);
-                for(i = 0; i < instruc.length; i++) {
-                    var insText = document.createElement("li");
-                    insText.textContent = instruc[i].step;
-                    list.appendChild(insText);
+                ingDiv.appendChild(list2Div);
+                container.appendChild(ingDiv);
+                var insTitle = document.createElement("p");
+                insTitle.setAttribute("class", "recipe-instruction-header");
+                insTitle.textContent ="Instructions";
+                container.appendChild(insTitle);
+                var inst = data.analyzedInstructions[0].steps; //analyzedInstructions[0].steps[0].step
+                var instructionDiv = document.createElement("div");
+                var insList = document.createElement("ul");
+               // insList.setAttribute("class", )
+                instructionDiv.setAttribute("class", "instruction-div");
+                for(i=0; i<inst.length; i++) {
+                    var instruction = document.createElement("li");
+                    instruction.textContent=inst[i].step;
+                    insList.appendChild(instruction);
+                }
+                instructionDiv.appendChild(insList);
+                container.appendChild(instructionDiv);
 
-                }
             });
         }
     })
@@ -94,5 +103,14 @@ if(response.ok) {
     });
     }; 
 
+    searchParam = () => {
+        var paramString = window.location;
+        var searchParams = new URLSearchParams(paramString);
+        var country = searchParams.get("search");
+        country = country.toString();
+       country = country.substring(2,country.length);
+        console.log(country);
+        getRecipeList(country);
+    };
 
-getRecipeList("vietnamese")
+searchParam();
