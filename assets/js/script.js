0831ba915e1a2a819ef71c1448d9e68229c7f38f
Logan Merchant <merchantclogan@gmail.com>
2020-09-19 16:34:03 -0600

    update spotify auth handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1015,6 +1015,6 @@ var displayFlag = function () {
 
 // End of Homepage Flag
 
-if (location.href.includes("music.html" || "access_token=")) {
+if (location.href.includes("music.html") || location.hash.includes("access_token=")) {
     tokenCheck();
 };
